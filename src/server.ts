0abf2e6f291b2db02e60c12b6841ff0f02984338
Joao Gomes <joaopfg.2002@gmail.com>
2021-07-20 22:53:08 +0100

    Fix: Refactor Routes

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,12 @@
 import express from 'express';
-import { categoriesRoutes } from './routes/categories.routes';
-import { specificationRoutes } from './routes/specifications.routes';
+import { router } from './routes';
+
 
 const app = express();
 app.use(express.json());
 
-app.use("/categories", categoriesRoutes)
-app.use("/specifications", specificationRoutes)
+app.use(router)
+
 
 
 

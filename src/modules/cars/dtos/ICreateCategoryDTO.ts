02eff2bd66c5921a0638783cf3d0b4b52ff7ee85
Joao Gomes <joaopfg.2002@gmail.com>
2021-07-21 23:44:53 +0100

    Merge

--- src/modules/cars/dtos/ICreateCategoryDTO.ts
+++ src/modules/cars/dtos/ICreateCategoryDTO.ts
@@ -1,4 +1,4 @@
 export interface ICreateCategoryDTO {
   name: string;
-  description : string;
+  description: string;
 }
--- src/modules/cars/dtos/ICreateSpecificationDTO.ts
+++ src/modules/cars/dtos/ICreateSpecificationDTO.ts
@@ -1,4 +1,4 @@
 export interface ICreateSpecificationDTO {
   name: string;
-  description : string;
+  description: string;
 }
